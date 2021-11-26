b581a5c8cceff344faee1a83310f9e313946c98f
porajap <poramintstyle@gmail.com>
2021-11-26 10:54:53 +0700

    p2 process, p2 page

--- p2.php
+++ p2.php
@@ -88,7 +88,6 @@ session_start();
 		</div>
 	</div>
 
-	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
 	<script>
 		checkEndDate();
 		const currentDateTime = new Date();
